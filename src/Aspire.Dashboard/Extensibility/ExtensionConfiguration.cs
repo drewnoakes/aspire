f78fd5ca081f6d63374340bc014fa145d55adf72
Drew Noakes <git@drewnoakes.com>
2024-09-12 14:39:21 +1000

    WIP

--- src/Aspire.Dashboard/Extensibility/ExtensionConfiguration.cs
+++ src/Aspire.Dashboard/Extensibility/ExtensionConfiguration.cs
@@ -28,26 +28,70 @@ public sealed class ExtensionConfiguration
 /// that's proffered by a dashboard extension.
 /// </summary>
 /// <remarks>
-/// Extensions do not have to provide top-level pages.
-/// Extensions may provide multiple top-level pages.
+/// An extension may provide zero or more top-level pages.
 /// </remarks>
 public sealed class TopLevelPageConfiguration
 {
+    /// <summary>
+    /// Gets a displayable title for the page.
+    /// </summary>
+    /// <remarks>
+    /// Used beneath the icon in the dashboard's navigation,
+    /// so should be quite a short string.
+    /// </remarks>
     [JsonPropertyName("title")]
     public required string Title { get; init; }
 
+    /// <summary>
+    /// Used in URLs to uniquely identify this top-level page.
+    /// </summary>
+    /// <remarks>
+    /// To view top-level pages, users navigate to a URL resembling
+    /// <c>http://localhost:1234/extension/my-extension</c>, where
+    /// the slug is <c>my-extension</c>.
+    /// </remarks>
     [JsonPropertyName("urlName")]
     public required string UrlSlug { get; init; }
 
+    /// <summary>
+    /// Gets the URL of the top-level page's content, to be hosted within
+    /// an iframe by the dashboard.
+    /// </summary>
+    /// <remarks>
+    /// This URL is not displayed to the user. The user sees a URL in their
+    /// browser that contains <see cref="UrlSlug"/>.
+    /// </remarks>
     [JsonPropertyName("targetUrl")]
     public required string TargetUrl { get; init; }
 
+    /// <summary>
+    /// Gets the name of the icon that represents this top-level page.
+    /// </summary>
     [JsonPropertyName("icon")]
     public string IconName { get; init; } = "PuzzlePiece";
 
+    /// <summary>
+    /// Gets the relative priority of this top-level page in lists presented to the user.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Top-level navigation items are ordered by increasing <see cref="Priority"/>,
+    /// then alphabetically by <see cref="Title"/>.
+    /// </para>
+    /// <para>
+    /// Defaults to zero.
+    /// </para>
+    /// </remarks>
     [JsonPropertyName("priority")]
     public int Priority { get; init; }
 
+    /// <summary>
+    /// Adds any validation errors to <paramref name="errors"/>.
+    /// </summary>
+    /// <param name="errors">
+    /// A reference to a list of errors. If an error is being added
+    /// and this value is null, it will be initialized.
+    /// </param>
     public void Validate(ref List<string>? errors)
     {
         if (string.IsNullOrWhiteSpace(Title))
